c08299e01647234c66b3d7f9ec9fdd37758f6b59
Marcin Marzeta <alfisti@post.pl>
2026-08-01 03:51:47 +0200

    0.1.1

--- obstat/__init__.py
+++ obstat/__init__.py
@@ -8,4 +8,4 @@ from .guard import Denied, Subject, guard, set_subject_resolver
 from .policy import PolicyError
 
 __all__ = ["Denied", "PolicyError", "Subject", "guard", "set_subject_resolver"]
-__version__ = "0.1.0"
+__version__ = "0.1.1"
